aade62626fde86d7546ee9c29ae2f7e84ba3faa3
Ghom <camille.abella@hotmail.fr>
2020-10-29 21:31:30 +0100

    fixed js.js and added my id :harold:

--- commands/js.js
+++ commands/js.js
@@ -1,9 +1,13 @@
 const deval = require("discord-eval.js")
 
 module.exports = function js(message) {
-  if(message.author.id !== "308540889754501120") return
-  const code = message.content.replace(/^js\s*/, "")
-  deval(code, message).catch(console.error)
+  if(
+    message.author.id !== "308540889754501120" && 
+    message.author.id !== "352176756922253321"
+  ) return;
+  deval(message.content, message).catch(console.error)
 }
+
+module.exports.aliases = ["eval", "code", "run"]
 
 module.exports.description = "Execute du Javascript"
