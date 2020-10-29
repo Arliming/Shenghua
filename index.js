d20b04c5a3066faeb17a414300fb6cc7411a9577
Ghom <camille.abella@hotmail.fr>
2020-10-29 20:57:03 +0100

    applied alias system and fixed command names

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const Discord = require("discord.js")
+const path = require("path")
+
 const { forFiles } = require("./utils.js")
 
-const Discord = require("discord.js")
-
 const client = new Discord.Client()
-
-const path = require("path")
 
 client.prefix = "a,"
 
@@ -13,7 +12,8 @@ client.commands = new Discord.Collection()
 forFiles(
   [path.join(__dirname, "commands")],
   function (filePath) {
-    client.commands.set(filePath, require(filePath))
+    const cmdName = path.basename(filePath, ".js")
+    client.commands.set(cmdName, require(filePath))
   }
 )
 
@@ -26,13 +26,17 @@ client.on("message", function (message) {
     return
   }
   // commande appelée
-  const command = client.commands.find((cmd, cmdPath) => {
-    const cmdName = path.basename(cmdPath, ".js")
-    return message.content.startsWith(cmdName)
+  const command = client.commands.find((cmd, cmdName) => {
+    const aliases = cmd.aliases ?? []
+    return (
+      message.content.startsWith(cmdName) ||
+      aliases.some(alias => message.content.startsWith(alias))
+    )
   })
-    command?.(message)
-
+  
+  command?.(message)
 })
 
-client.login("NzcwNzY5MzQwNjkzMjE3Mjgx.X5iY4w.sJ7c98cPWSaj-Dfqv4SHYqgSaHo")
+client.login(require("./token/json"))
+    
 //https://discord.com/oauth2/authorize?client_id=770769340693217281&scope=bot&permissions=8
