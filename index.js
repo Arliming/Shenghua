aedca7ad755a4827fac2e2d445d1daae1dae2ee1
Ghom <camille.abella@hotmail.fr>
2020-10-29 21:29:08 +0100

    add message event on indeX.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-require("./client.js")
+const client = require("./client.js")
+
+const { findCommand } = require("./utils.js")
+
+client.on("message", function (message) {
+  if(message.system || message.author.bot) return
+  // prefix check
+  if(message.content.startsWith(client.prefix)){
+    message.content = message.content.slice(client.prefix.length)
+  }else{
+    return
+  }
+  // commande appelée
+  const command = findCommand(message.content)
+  
+  if(command){
+    message.content = message.content.slice(command.name.length).trim()
+    command(message)
+  }
+})
     
 //https://discord.com/oauth2/authorize?client_id=770769340693217281&scope=bot&permissions=8
