a6010364563e494818e6212825e4c9590fe81469
Arliming <53376245+Arliming@users.noreply.github.com>
2020-10-30 22:47:36 +0100

    update

--- commands/invite.js
+++ commands/invite.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js")
 
-module.exports = function invite(message){
+module.exports = function invite(message, member){
 
     const embed = new Discord.MessageEmbed()
 
       .setTitle("Voici le lien d'invitation de Shēnghuá")
       .setDescription("[>>Invite moi<<](https://discord.com/oauth2/authorize?client_id=770769340693217281&scope=bot&permissions=8)")
-      .setColor(message.client.roleHexColor)
-      .setAuthor(message.author.tag, message.author.avatarURL)
-      .setThumbnail(message.client.user.avatarURL)
+      .setColor(message.guild?.me.roles.color?.color ?? "#c800ff")
+      .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+      .setThumbnail(message.client.user.displayAvatarURL({ dynamic: true }))
       .setTimestamp();
 
     message.channel.send(embed)
