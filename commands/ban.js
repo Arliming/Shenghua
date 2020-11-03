00702b215a075415fbc728640cb46e14106924a9
Arliming <53376245+Arliming@users.noreply.github.com>
2020-11-03 19:28:33 +0100

    update

--- commands/ban.js
+++ commands/ban.js
@@ -2,23 +2,39 @@ module.exports = function ban(message){
     const {member, mentions} = message
 
     const tag = `<@${member.id}>`
+    const target = mentions.users.first()
+    const targetMember = message.guild.members.cache.get(target.id)
+    const grade = member.roles.highest.comparePositionTo(targetMember.roles.highest)
 
+    //Permission de l'auteur
     if (
         member.hasPermission('ADMINISTRATOR') ||
         member.hasPermission('BAN_MEMBERS')
     ) {
-        const target = mentions.users.first()
-        if (target) {
-            const targetMember = message.guild.members.cache.get(target.id)
-            targetMember.ban()
-            message.channel.send(`${tag} Cet utilisateur a été ban`)
+
+        //rang de l'auteur supérieur à la cible ?
+        if (
+            grade > 0
+        ) {
+
+            //Permission du bot
+            if (targetMember.bannable) {
+        
+                if (target) {
+                    targetMember.ban()
+                    message.channel.send(`${tag} Cet utilisateur a été ban`)
+
+                } else {
+                    message.channel.send(`${tag} Précise qui tu veux ban ?`)
+                }
+            } else {
+                message.channel.send(`${tag} Je ne possède pas la permission :/`)
+            }
         } else {
-            message.channel.send(`${tag} Précise qui tu veux ban ?`)
+            message.channel.send(`${tag} Vous ne pouvez pas ban un utilisateur plus haut gradé ou du même grade que vous !`)
         }
     } else {
-        message.channel.send(
-            `${tag} Vous n'avez pas les permissions !`
-        )
+        message.channel.send(`${tag} Vous n'avez pas les permissions !`)
     }
 }
 
