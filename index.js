b31204beb83a0d3aff8f0d9e2bdfc295e69d7170
Arliming <53376245+Arliming@users.noreply.github.com>
2020-11-19 14:00:43 +0100

    je trop conne pardon

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const Akairo = require("discord-akairo")
 const client = new Discord.Client()
 const akairo = new Akairo.Framework(client, {
   ownerID: "ID",
-  prefix: "!",
-  commandDirectory: "./src/commands/",
-  inhibitorDirectory: "./src/inhibitors/",
-  listenerDirectory: "./src/listeners/",
+  prefix: "a,",
+  commandDirectory: "./commands/",
 })
 
 akairo.login(require("./token.json")).then(() => {
-  console.log("Started up!")
+  console.log("I'm back !")
 })
