41660a6f077c47efdc2b9e3840f3865987fe9af8
Ghom <camille.abella@hotmail.fr>
2020-11-19 23:31:32 +0100

    added deploy command

---
+++ commands/deploy.js
@@ -0,0 +1,41 @@
+const { Command } = require("discord-akairo")
+cosnt exec = require("util")
+  .promisify(require("child_process").exec)
+
+module.exports = class extends Command {
+  constructor() {
+    super("deploy", {
+      ownerOnly: true,
+      channel: "guild",
+      cooldown: 1000 * 60 * 5,
+      aliases: ["deploy", "reload"],
+      category: "Secret",
+      description: "Déploie en production le dernier push",
+    })
+  }
+  
+  async exec(message){
+    const subject = await message.channel.send(
+      "<a:wait:560972897376665600> En cours de déploiement..."
+    )
+    const timer = Date.now()
+
+    try {
+      await exec("git pull && npm i && npm run build")
+      await subject.edit(
+        `Déploiement réussi ! <:yay:557124850326437888>\n*Effectué en ${
+          Date.now() - timer
+        }ms*`
+      )
+      app.globals.set("helloChannel", message.channel.id)
+      process.exit(0)
+    } catch (error) {
+      await subject.edit(
+        `Une erreur est survenue lors du déploiement <:why:557124850422906880>\nGo le faire à la main... ${app.code(
+          `${error.name}: ${error.message}`,
+          ""
+        )}`
+      )
+    }
+  }
+}
